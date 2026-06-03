958a8e21ab8d7d1c17227ac618595a2fe9dac859
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-03 17:13:42 +0900

    answer2 No3

--- src/main/scala/education/section1/Answer2.scala
+++ src/main/scala/education/section1/Answer2.scala
@@ -1,10 +1,11 @@
 package education.section1
 
 object Answer2:
-  val Pi: Double = 3.14
+  val PI: Double = 3.14
   def main(args: Array[String]): Unit =
     println(circleArea(2.0))
 
   def circleArea(r: Double): Double =
-    Pi * r * r
+    PI * r * r
+
 
