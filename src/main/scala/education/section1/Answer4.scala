e960019152f6aee5da272fe18e21ba1978bbae8a
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-03 17:59:07 +0900

    answer4

---
+++ src/main/scala/education/section1/Answer4.scala
@@ -0,0 +1,11 @@
+package education.section1
+
+object Answer4:
+  def main(args: Array[String]): Unit =
+    println(introduce("Taro", 25))
+
+  def introduce(name: String, age: Int): String =
+    s"私は${name}です。${age}歳です。"
+
+
+
