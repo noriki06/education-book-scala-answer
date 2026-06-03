91f1961a1efaf305fa981e9441506600d12be0db
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-03 18:58:50 +0900

    answer6

---
+++ src/main/scala/education/section1/Answer6.scala
@@ -0,0 +1,12 @@
+ package education.section1
+
+object Answer6:
+  val r = Rectangle(3, 4)
+  def main(args: Array[String]): Unit =
+    println(r.area)
+
+  class Rectangle(width: Int, height: Int):
+
+    def area: Int = width * height
+
+
