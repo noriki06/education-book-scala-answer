b55a79bc2e4a1b7986c45c4724c233c833f61975
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-04 15:04:39 +0900

    PreferVal2.scala.No2

--- src/main/scala/education/section1/PreferVal2.scala
+++ src/main/scala/education/section1/PreferVal2.scala
@@ -3,11 +3,14 @@ object PreferVal2:
   def main(args: Array[String]): Unit =
     println(new Counter(0).increment.increment.count)
   
+  //例＜
   class CounterMutable:
     var count: Int = 0
     def increment(): Unit =
-  
       count = count + 1
+
+    //＞例
+
   class Counter (val count: Int = 0):
     def increment: Counter =
       new Counter (count + 1)
