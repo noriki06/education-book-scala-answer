f22f718b78861a75efd17648c3e7369694cda19c
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-04 11:25:20 +0900

    PreferVal1.scala.No2

--- src/main/scala/education/section1/PreferVal1.scala
+++ src/main/scala/education/section1/PreferVal1.scala
@@ -2,7 +2,5 @@ package education.section1
 
 object PreferVal1:
   def main(args: Array[String]): Unit =
-    var price = 1000
-    price = price + 500   // 送料を足す
-    price = price - 200   // クーポンで値引き
+    var price = 1000 + 500 - 200
     println(price)
