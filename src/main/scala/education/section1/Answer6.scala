a573bd0eec3764c74a98995323a6aa6877b8c7aa
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-04 10:35:31 +0900

    answer6

--- src/main/scala/education/section1/Answer6.scala
+++ src/main/scala/education/section1/Answer6.scala
@@ -1,9 +1,10 @@
 package education.section1
 
 object Answer6:
-  val r = Rectangle(3, 4)
   def main(args: Array[String]): Unit =
     println(r.area)
 
   class Rectangle(width: Int, height: Int):
     def area: Int = width * height
+  
+  val r = Rectangle(3, 4)
