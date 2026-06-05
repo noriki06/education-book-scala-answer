6ad86039d6bf0b31049d55ca1fc1c0be831c85cb
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-05 15:06:18 +0900

    section2AnswerEx3

---
+++ src/main/scala/education/section2/AnswerEx3.scala
@@ -0,0 +1,17 @@
+package education.section2
+
+object AnswerEx3:
+
+  def divide(a: Int, b: Int): Option[Int] =
+    if b == 0 then None
+    else Some(a / b)
+
+  def divChain(a: Int, b: Int, c: Int): Option[Int] =
+    
+    if c == 0 then None
+    else divide(a, b).flatMap(ab => divide(ab, c))
+
+  def main(args: Array[String]): Unit =
+    println(divChain(20, 2, 5))
+    println(divChain(20, 0, 5))
+    println(divChain(20, 2, 0))
