ca59f644f7fe3c9e729c89ffc092208717fa2c44
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-08 12:29:13 +0900

    section3Answer4 2

--- src/main/scala/education/section3/Answer4.scala
+++ src/main/scala/education/section3/Answer4.scala
@@ -4,8 +4,5 @@ object Answer4:
   def seqQuestion4(strSeq: Seq[String]): Seq[String] =
     strSeq.collect { case s if s.length >= 2 => s + "x" }
 
- 
-
-
   def main(args: Array[String]): Unit =
     println(seqQuestion4(Seq("a", "bb", "ccc")))
--- src/main/scala/education/section3/Answer7.scala
+++ src/main/scala/education/section3/Answer7.scala
@@ -1,9 +1,5 @@
-package education.section3
-
-object Answer7:
-  def seqQuestion7(numSeq: Seq[Int]): Int =
 
 
-  def main(args: Array[String]): Unit =
-    println(seqQuestion7(Seq(1, 2, 3, 4)))
-    println(seqQuestion6(Seq())
+
+
+
