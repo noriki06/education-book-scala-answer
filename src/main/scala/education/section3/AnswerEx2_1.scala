bda7fc68be5020eba90f89bc18298eb83d2f2bf3
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-08 18:11:46 +0900

    section3AnswerEx2_1

---
+++ src/main/scala/education/section3/AnswerEx2_1.scala
@@ -0,0 +1,17 @@
+package education.section3
+
+class Card(val suit: String, val number: Int)
+object AnswerEx2_1:
+// 種類（suit: ♠ ♥ ♦ ♣ などのマーク）と数字（number）を持つ 1 枚のカード
+  def sortedLabels(cards: Seq[Card]): Seq[String]  =
+    cards.sortBy(_.number).map(t => s"${t.suit}${t.number}")
+      //数値で昇順でならぶ＞suit, numberをs"${suit}${number}"
+      //seq[String]
+  def main(args: Array[String]): Unit =
+    val hand: Seq[Card] = Seq(
+      Card("♠", 10),
+      Card("♥", 3),
+      Card("♦", 7)
+    )
+      //seq[Card]
+    println(sortedLabels(hand))
