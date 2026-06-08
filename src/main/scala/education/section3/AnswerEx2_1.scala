d1c24a2036de46bb7a9920ac2a9ee9e71a9ddcf7
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-08 19:04:59 +0900

    section3AnswerEx2_1

--- src/main/scala/education/section3/AnswerEx2_1.scala
+++ src/main/scala/education/section3/AnswerEx2_1.scala
@@ -1,12 +1,12 @@
 package education.section3
 
 class Card(val suit: String, val number: Int)
+
 object AnswerEx2_1:
-// 種類（suit: ♠ ♥ ♦ ♣ などのマーク）と数字（number）を持つ 1 枚のカード
+
   def sortedLabels(cards: Seq[Card]): Seq[String]  =
     cards.sortBy(_.number).map(t => s"${t.suit}${t.number}")
-      //数値で昇順でならぶ＞suit, numberをs"${suit}${number}"
-      //seq[String
+   
   def totalNumber(cards: Seq[Card]): Int = 
     cards.map(_.number).sum
 
@@ -16,6 +16,5 @@ object AnswerEx2_1:
       Card("♥", 3),
       Card("♦", 7)
     )
-      //seq[Card]
     println(sortedLabels(hand))
     println(totalNumber(hand))
