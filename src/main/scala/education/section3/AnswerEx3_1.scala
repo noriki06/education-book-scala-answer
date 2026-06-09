0ff604d67faae722682389e88a59bc6d65f048ef
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-09 19:23:24 +0900

    section3AnswerEx3_1

--- src/main/scala/education/section3/AnswerEx3_1.scala
+++ src/main/scala/education/section3/AnswerEx3_1.scala
@@ -24,57 +24,42 @@ object AnswerEx3_1:
 
   def main(args: Array[String]): Unit =
     val trainers: Seq[Trainer] = Seq(
-      Trainer(
-        "サトシ",
-        Seq(
-          Pokemon("ピカチュウ", "ぴかちゅう", 35, 35,
-            Seq(
-              Skill("電光石火", "でんこうせっか", "攻撃",40),
-              Skill("自己再生", "じこさいせい", "回復", 50)
+      Trainer("サトシ",Seq(
+          Pokemon("ピカチュウ", "ぴかちゅう", 35, 35, Seq(
+            Skill("電光石火", "でんこうせっか", "攻撃",40),
+            Skill("自己再生", "じこさいせい", "回復", 50)
           ) ),
-          Pokemon("リザードン", "りざーどん", 78, 78,
-            Seq(
+          Pokemon("リザードン", "りざーどん", 78, 78, Seq(
               Skill("火炎放射", "かえんほうしゃ", "攻撃",90),
               Skill("破壊光線", "はかいこうせん", "攻撃", 150)
-          ) )
-          Pokemon("カビゴン", "かびごん", 160, 160,
-            Seq(
+          ) ),
+          Pokemon("カビゴン", "かびごん", 160, 160,Seq(
               Skill("地震", "じしん", "攻撃",100),
               Skill("回復", "かいふく", "回復", 60)
-          ) )
-        ),
-        "カスミ",
-        Seq(
-          Pokemon("ゼニガメ", "ぜにがめ", 44, 44,
-            Seq(
+      ) ) ) ),
+      Trainer("カスミ", Seq(
+          Pokemon("ゼニガメ", "ぜにがめ", 44, 44, Seq(
               Skill("水鉄砲", "でんこうせっか", "攻撃",40),
               Skill("甲羅休め", "じこさいせい", "回復", 50)
           ) ),
-          Pokemon("フシギバナ", "りざーふしぎばな", 80, 80,
-            Seq(
+          Pokemon("フシギバナ", "りざーふしぎばな", 80, 80, Seq(
               Skill("草結び", "くさむすび", "攻撃",65),
               Skill("光合成", "こうごうせい", "回復", 70)
-          ) )
-        ),
-        "タケシ",
-        Seq(
-          Pokemon("ゼニガメ", "ぜにがめ", 44, 44,
-            Seq(
+      ) ) ) ),
+      Trainer("タケシ", Seq(
+          Pokemon("ゼニガメ", "ぜにがめ", 44, 44, Seq(
               Skill("水鉄砲", "みずでっぽう	", "攻撃",45),
               Skill("甲羅休め", "こうらやすめ", "回復", 40)
           ) ),
-          Pokemon("イワーク", "いわーく", 55, 55,
-            Seq(
+          Pokemon("イワーク", "いわーく", 55, 55, Seq(
               Skill("岩石封じ", "がんせきふうじ	", "攻撃",60),
               Skill("地割れ", "じわれ", "攻撃", 120)
           ) ),
-          Pokemon("イシツブテ", "いしつぶて", 40, 40,
-            Seq(
+          Pokemon("イシツブテ", "いしつぶて", 40, 40, Seq(
               Skill("落石", "らくせき", "攻撃",50),
               Skill("治療", "ちりょう", "回復", 30)
     ) ) ) ) )
     println(showAllSkills(trainers))
 
   def showAllSkills(trainers: Seq[Trainer]): Unit =
-    trainers.map(s => s.skillName).distinct.sorted
-
+    trainers.map(t => t.holdsPokemon).map(p => p.rememberedTechniques)
