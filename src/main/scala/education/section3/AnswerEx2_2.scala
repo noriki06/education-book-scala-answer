d73f72c5374927fd8225ef09996387d668ea6f7f
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-09 11:23:21 +0900

    section3AnswerEx2_2

--- src/main/scala/education/section3/AnswerEx2_2.scala
+++ src/main/scala/education/section3/AnswerEx2_2.scala
@@ -1,1 +1,25 @@
+package education.section3
+
+class Student(val name: String, val score: Int)
+
+object AnswerEx2_2:
+
+  def ranking(students: Seq[Student]): Seq[String] =
+    students.sortBy(s => -s.score).map(s => s.name)
+
+  def averageScore(students: Seq[Student]): Double =
+    if students.isEmpty then 0.0
+    else students.map(s => s.score).sum.toDouble / students.size
+
+    
+
+  def main(args: Array[String]): Unit =
+    val students: Seq[Student] = Seq(
+      Student("Alice", 80),
+      Student("Bob",   95),
+      Student("Carol", 70)
+    )
+
+    println(ranking(students))
+    println(averageScore(students))
 
