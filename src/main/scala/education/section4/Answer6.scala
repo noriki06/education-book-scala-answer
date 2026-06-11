4e342bf0d435336b87b9896858e9efc5d2a08157
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-11 15:07:59 +0900

    section4Answer4,5,63

--- src/main/scala/education/section4/Answer6.scala
+++ src/main/scala/education/section4/Answer6.scala
@@ -6,8 +6,7 @@ object Answer6:
   def caseClassQuestion3(item: Item): Item =
     item.copy(price = item.price * 2)
 
-
-
   def main(args: Array[String]): Unit =
+    println(caseClassQuestion3(Item("apple", 120)))
     println(caseClassQuestion3(Item("apple", 120)))
 
