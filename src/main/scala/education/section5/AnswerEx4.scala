577da3cfa2825c842b6bca20db1908aa75b444e4
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-12 12:44:14 +0900

    section5AnswerEx1,2,3

---
+++ src/main/scala/education/section5/AnswerEx4.scala
@@ -0,0 +1,9 @@
+//package education.section5
+
+//object AnswerEx4:
+//
+  //def msEx4(prices: Map[String, Int], order: Seq[String]): Int =
+    //pairs.toMap
+
+  //def main(args: Array[String]): Unit =
+    //println(msEx4(Map("apple" -> 120, "banana" -> 200), Seq("apple", "banana", "apple", "melon")))
