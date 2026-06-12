a33f2a33315099f4ae848b67e496c66a19927745
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-12 15:31:47 +0900

    section5AnswerEx4,5,62

--- src/main/scala/education/section5/AnswerEx5.scala
+++ src/main/scala/education/section5/AnswerEx5.scala
@@ -4,7 +4,7 @@ object AnswerEx5:
   case class Student(name: String, team: String, score: Int)
 
   def msEx5(students: Seq[Student]): Map[String, Int] =
-    students.groupMap(t => t.team)(t => t.score).view.mapValues(score => score.sum).toMap 
+    students.groupMap(student => student.team)(t => t.score).view.mapValues(scores => scores.sum).toMap 
 
   def main(args: Array[String]): Unit =
     println(msEx5(Seq(Student("Alice", "A", 80), Student("Bob", "B", 95), Student("Carol", "A", 60))))
--- src/main/scala/education/section5/AnswerEx6.scala
+++ src/main/scala/education/section5/AnswerEx6.scala
@@ -3,8 +3,8 @@ package education.section5
 object AnswerEx6:
 
   def msEx6(sales: Map[String, Int]): Seq[(String, Int)] =
-    sales.toSeq.sortBy(s => -s._2)
-
+    case (name, amount) => -amount
+    sales.toSeq.sortBy { case (name, amount) => -amount }
 
   def main(args: Array[String]): Unit =
     println(msEx6(Map("apple" -> 300, "banana" -> 1200, "kiwi" -> 800)))
