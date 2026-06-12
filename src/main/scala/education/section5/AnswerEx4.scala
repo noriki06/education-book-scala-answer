97c1493c01e750a75a38c827f4df2cd8965e16c6
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-12 13:23:49 +0900

    section5AnswerEx4

--- src/main/scala/education/section5/AnswerEx4.scala
+++ src/main/scala/education/section5/AnswerEx4.scala
@@ -3,7 +3,7 @@ package education.section5
 object AnswerEx4:
 
   def msEx4(prices: Map[String, Int], order: Seq[String]): Int =
-    pairs.toMap
+    order.toSeq(prices._1 => prices.get(prices._1).getOrElse(0))
 
   def main(args: Array[String]): Unit =
     println(msEx4(Map("apple" -> 120, "banana" -> 200), Seq("apple", "banana", "apple", "melon")))
