0d47bded42c3e83f5e27a385c0dc61b9b511bc7c
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-16 11:28:46 +0900

    section6AnswerCheckpoint1Library

---
+++ src/main/scala/education/section6/AnswerCheckpoint2Company.scala
@@ -0,0 +1,52 @@
+package education.section6
+
+object AnswerCheckpoint2Company:
+
+  case class Department(
+    id: Department.Id,
+    name: String
+  )
+
+  object Department:
+    opaque type Id = Long
+    object Id:
+      def apply(value: Long): Id = value
+
+  case class Employee(
+    id: Employee.Id,
+    name: String,
+    departmentId: Department.Id,
+    salary: Int,
+    employmentType: Employee.EmploymentType
+  )
+
+  object Employee:
+    opaque type Id = Long
+    object Id:
+      def apply(value: Long): Id = value
+
+    enum EmploymentType:
+      case FullTime,
+           Contract,
+           PartTime
+
+  def main(args: Array[String]): Unit =
+    println(departments)                               // val customers(顧客情報)の内容が正しいかを出力
+    println(employees)
+
+  val departments: Seq[Department] =
+    Seq(
+      Department(Department.Id(1), "営業部"),
+      Department(Department.Id(2), "開発部"),
+      Department(Department.Id(3), "人事部"),
+      Department(Department.Id(4), "広報部")
+    )
+
+  val employees: Seq[Employee] =
+    Seq(
+      Employee(Employee.Id(1001), "Alice", Department.Id(2), 50, Employee.EmploymentType.FullTime),
+      Employee(Employee.Id(1002), "Bob",   Department.Id(1), 40, Employee.EmploymentType.Contract),
+      Employee(Employee.Id(1003), "Carol", Department.Id(2), 60, Employee.EmploymentType.FullTime),
+      Employee(Employee.Id(1004), "Dave",  Department.Id(1), 45, Employee.EmploymentType.FullTime),
+      Employee(Employee.Id(1005), "Eve",   Department.Id(2), 55, Employee.EmploymentType.PartTime)
+    )
