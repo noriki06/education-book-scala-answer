e62faeee7211b7eef1d6731e1b9bd99335b84611
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-18 17:08:48 +0900

    section8

--- src/main/scala/education/section8/AnswerEx1.scala
+++ src/main/scala/education/section8/AnswerEx1.scala
@@ -10,31 +10,31 @@ object AnswerEx1:
     age:  Int
   )
 
-    /**
-     *名前の検証
-     */
-    def validateName(name: String): Either[String, String] =
-      name match
-        case name if name == "" => Left("名前が空です")
-        case name               => Right(name)
+  /**
+    *名前の検証
+    */
+  def validateName(name: String): Either[String, String] =
+    name match
+      case name if name == "" => Left("名前が空です")
+      case name               => Right(name)
 
-    /**
-     *年齢の検証
-     */
-    def validateAge(age: Int): Either[String, Int] =
-      age match
-        case age if age < 0   => Left("年齢が不正です")
-        case age              => Right(age)
-    /**
-     *ユーザー情報の検証
-     */
-    def validateUser(name: String, age: Int): Either[String, User] =
-      for{
-        a <- validateName(name)
-        b <- validateAge(age)
-      }yield User(a, b)
+  /**
+    *年齢の検証
+    */
+  def validateAge(age: Int): Either[String, Int] =
+    age match
+      case age if age < 0   => Left("年齢が不正です")
+      case age              => Right(age)
+  /**
+    *ユーザー情報の検証
+    */
+  def validateUser(name: String, age: Int): Either[String, User] =
+    for{
+      a <- validateName(name)
+      b <- validateAge(age)
+    }yield User(a, b)
 
-    def main(args: Array[String]): Unit =
-      println(validateUser("Alice", 20))
-      println(validateUser("", 20))
-      println(validateUser("Bob", -1))
+  def main(args: Array[String]): Unit =
+    println(validateUser("Alice", 20))
+    println(validateUser("", 20))
+    println(validateUser("Bob", -1))
