148b9c0f88380ef374982213c8b2e1d452a94b0a
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-19 08:33:35 +0900

    section8_3

--- src/main/scala/education/section8/AnswerEx3.scala
+++ src/main/scala/education/section8/AnswerEx3.scala
@@ -1,21 +1,21 @@
-//package education.section8
+package education.section8
 
-//object AnswerEx3:
+object AnswerEx3:
 
-  //import scala.util.{Try, Success, Failure}
+  import scala.util.{Try, Success, Failure}
 
-  //def parse(s: String): Either[String, Int] =
-    //Try(s.toInt)
-      //.toEither
-      //.left.map(e => s"数値に変換できません: + $e")
+  def parse(s: String): Either[String, Int] =
+    Try(s.toInt)
+      .toEither
+      .left.map(e => s"数値に変換できません: + $e")
 
-  //def sumThree(a: String, b: String, c: String): Either[String, Int] =
-    //for{
-      //x <- parse(a)
-      //y <- parse(b)
-      //z <- parse(c)
-    //}yield x + y + z
+  def sumThree(a: String, b: String, c: String): Either[String, Int] =
+    for{
+      x <- parse(a)
+      y <- parse(b)
+      z <- parse(c)
+    }yield x + y + z
 
- //def main(args: Array[String]): Unit =
-    //println(sumThree("1", "2", "3"))
-    //println(sumThree("1", "x", "3"))
+  def main(args: Array[String]): Unit =
+    println(sumThree("1", "2", "3"))
+    println(sumThree("1", "x", "3"))
