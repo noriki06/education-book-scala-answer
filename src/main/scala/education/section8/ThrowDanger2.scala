50c0851533b5f6465e077b348d2f0633c479acf6
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-18 13:22:05 +0900

    section8

--- src/main/scala/education/section8/ThrowDanger2.scala
+++ src/main/scala/education/section8/ThrowDanger2.scala
@@ -2,15 +2,17 @@ package education.section8
 
 object ThrowDanger2:
   // 文字列を数値にする。失敗すると例外を投げる（s.toInt がそうなっている）
-  def parseInt(s: String): Int =
-    import scala.util.Try
-    val result: Try[Int] = Try(s.toInt)
+  def parseInt(s: String): Try[Int] =
+    Try(s.toInt)
 
+  def main(args: Array[String]): Unit =
+    import scala.util.{Try, Success, Failure}
+
+    val result: Try[Int] = parseInt()
     result match
       case Success(n) => n
       case Failure(e) => "(変換できません)"
 
-  def main(args: Array[String]): Unit =
     println(parseInt("42"))    // 数値 → 42
     println(parseInt("abc"))   // 数値でない → ここで例外が発生
     println("最後まで到達した")   // この行は表示される？
