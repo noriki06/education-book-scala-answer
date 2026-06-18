8edbe7618b31b2f13d0b07834cac4e264d3abfe7
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-18 13:02:29 +0900

    section8

---
+++ src/main/scala/education/section8/ThrowDanger2.scala
@@ -0,0 +1,16 @@
+package education.section8
+
+object ThrowDanger2:
+  // 文字列を数値にする。失敗すると例外を投げる（s.toInt がそうなっている）
+  def parseInt(s: String): Int =
+    import scala.util.Try
+    val result: Try[Int] = Try(s.toInt)
+
+    result match
+      case Success(n) => n
+      case Failure(e) => "(変換できません)"
+
+  def main(args: Array[String]): Unit =
+    println(parseInt("42"))    // 数値 → 42
+    println(parseInt("abc"))   // 数値でない → ここで例外が発生
+    println("最後まで到達した")   // この行は表示される？
