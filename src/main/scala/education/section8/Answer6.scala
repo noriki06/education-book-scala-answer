2a6a26b1de2b69ae77e084ba5ea3b30070b834e8
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-18 16:05:18 +0900

    section8

--- src/main/scala/education/section8/Answer6.scala
+++ src/main/scala/education/section8/Answer6.scala
@@ -1,1 +1,25 @@
+package education.section8
 
+object Answer6:
+
+  import scala.util.{Try, Success, Failure}
+
+  def tryQuestion5(s: String): Try[Int] =
+    Try(s.toInt)
+
+  def tryQuestion6(a: String, b: String): Try[Int] =
+    for {
+      x <- a.tryQuestion5
+      y <- b.tryQuestion5
+    } yield x * y
+
+
+  def main(args: Array[String]): Unit =
+    println(tryQuestion6(a = "3", b = "4") match
+      case Success(n) => s"Success($n)"
+      case Failure(s) => s"Failure($s)"
+    )
+    println(tryQuestion6(a = "3", b = "y:") match
+      case Success(n) => s"Success($n)"
+      case Failure(s) => s"Failure($s)"
+    )
