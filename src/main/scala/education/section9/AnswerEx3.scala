7e6c83db14e64d23ad223dcbd32cc73fb06cbfa4
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-19 18:06:04 +0900

    section9Answer2

---
+++ src/main/scala/education/section9/AnswerEx3.scala
@@ -0,0 +1,20 @@
+package education.section9
+
+object AnswerEx3:
+
+  import scala.concurrent.{Future, Await}
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.duration.*
+
+  def fetch(id: Int): Future[String] =
+    Future {
+      s"item$id"
+    }
+
+  def fetchAll(ids: Seq[Int]): Future[Seq[String]] =
+    Future.sequence(ids.map(fetch))
+
+
+  def main(args: Array[String]): Unit =
+    val result: Seq[String] = Await.result(fetchAll, Duration.Inf)
+    println(result)
