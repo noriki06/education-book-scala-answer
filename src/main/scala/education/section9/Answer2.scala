4984c91957bce2206901772d6a336cbbb8140c4a
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-19 16:29:46 +0900

    section9Answer1

--- src/main/scala/education/section9/Answer2.scala
+++ src/main/scala/education/section9/Answer2.scala
@@ -5,8 +5,6 @@ object Answer2:
   import scala.concurrent.Future
   import scala.concurrent.ExecutionContext.Implicits.global
   import scala.util.{Success, Failure}
-  import scala.concurrent.Await
-  import scala.concurrent.duration.*
 
   def futureQuestion2(s: String): Future[Int] =
     Future {
@@ -15,11 +13,14 @@ object Answer2:
 
 
   def main(args: Array[String]): Unit =
-    val futureResult1: Future[Int] =
-      futureQuestion2("123")
-        .onComplete {
-          case Success(n) => println(n)
-          case Failure(e) => println(s"失敗: $e")
-        }
-    val result1: Int = Await.result(futureResult1, Duration.Inf)
-    println(result1)
+    futureQuestion2("123").onComplete {
+      case Success(n) => println(n)
+      case Failure(e) => println(s"失敗: $e")
+    }
+
+    futureQuestion2("abc").onComplete {
+      case Success(n) => println(n)
+      case Failure(e) => println(s"失敗: $e")
+    }
+
+    Thread.sleep(1000)
