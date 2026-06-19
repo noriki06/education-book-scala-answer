f0de802d26ccb1301e31c20e47eb85f150fb6ef8
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-19 17:14:10 +0900

    section9Answer1

--- src/main/scala/education/section9/AnswerEx1.scala
+++ src/main/scala/education/section9/AnswerEx1.scala
@@ -7,14 +7,22 @@ object AnswerEx1:
   import scala.concurrent.duration.*
 
   def slow(label: String, value: Int): Future[Int] =
-    for {
-      a <- slow(...)
-      b <- slow(...)
-    } yield a + b
+    Future {
+      println(s"[$label] 開始")
+      Thread.sleep(1000) // 1秒待つ処理をシミュレート
+      println(s"[$label] 終了")
+      value
+    }
 
+  def main(args: Array[String]): Unit =
+    val f1 = slow("一つ目の処理", 10)
+    val f2 = slow("二つ目の処理", 20)
 
-    Thread.sleep(1000)
+    val combinedFuture: Future[Int] =
+      for {
+        result1 <- f1
+        result2 <- f2
+      } yield result1 + result2
 
-
-
-    def main(args: Array[String]): Unit =
+    val finalResult: Int = Await.result(combinedFuture, Duration.Inf)
+    println(s"合計結果: $finalResult")
