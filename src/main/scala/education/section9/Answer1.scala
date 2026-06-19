46d7fd8b29ceaa4d639412b6c63bc8e580e2a9a5
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-19 15:48:08 +0900

    section9Answer1

---
+++ src/main/scala/education/section9/Answer1.scala
@@ -0,0 +1,18 @@
+package education.section9
+
+object Answer1:
+
+  import scala.concurrent.Future
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.Await
+  import scala.concurrent.duration.*
+
+  def futureQuestion1(n: Int): Future[Int] =
+    Future {
+      n * 2
+    }
+
+  def main(args: Array[String]): Unit =
+    val resultFuture: Future[Int] = futureQuestion1(21)
+    val result: Int = Await.result(resultFuture, Duration.Inf)
+    println(futureQuestion1(21))
