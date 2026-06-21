6c219c1372fa5815c72be8c9c7ab449282791315
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-21 17:08:56 +0900

    section9AnswerCheckpointFuture

---
+++ src/main/scala/education/section9/AnswerCheckpointFuture.scala
@@ -0,0 +1,7 @@
+package education.section9
+
+object AnswerCheckpointFuture:
+
+  import scala.concurrent.{Future, Await}
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.duration.*
