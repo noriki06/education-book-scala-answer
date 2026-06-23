fe9647e858025e0afde2f84f0703f731d96fbb8d
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-23 11:30:33 +0900

    section3AnswerEx3_6

--- src/main/scala/education/section3/AnswerEx3_6.scala
+++ src/main/scala/education/section3/AnswerEx3_6.scala
@@ -1,31 +1,32 @@
 package education.section3
 
 object AnswerEx3_6:
-  val MATH_RANDOM = new scala.util.Random(256)
+val MATH_RANDOM = new scala.util.Random(256)
+
 
   // ポケモンの技のケースクラス
   case class PokemonSkill(
-    val name: String,          // 技名
-    val pronunciation: String, // よみがな
-    val kind: String,          // 種別
-    val power: Int             // 威力
+    val name: String,//技名
+    val pronunciation: String,//よみがな
+    val kind: String,//種別
+    val power: Int//威力
   )
   // ポケモンのケースクラス
   case class Pokemon(
-    val name: String,            // ポケモン名
-    val pronunciation: String,   // よみがな
-    val hpMax: Int,              // 最大 HP
-    val hp: Int,                 // 現在 HP
-    val skill: Seq[PokemonSkill] // 覚えている技
+    val name: String,//ポケモン名
+    val pronunciation: String,//よみがな
+    val hpMax: Int,//最大 HP
+    val hp: Int,//現在 HP
+    val skill: Seq[PokemonSkill]//覚えている技
   )
 
   // トレーナーのケースクラス
   case class Trainer(
-    val name: String,              // トレーナー名
-    val holdsPokemon: Seq[Pokemon] // 手持ちポケモン
+    val name: String,//トレーナー名
+    val holdsPokemon: Seq[Pokemon]//手持ちポケモン
   )
 
-  val trainers: Seq[Trainer] = Seq(
+  val trainers: Seq[Trainer] = Seq( // トレーナーデータ
     Trainer("サトシ",Seq(
       Pokemon("ピカチュウ", "ぴかちゅう", 35, 35, Seq(
         PokemonSkill("電光石火", "でんこうせっか", "攻撃",40),
@@ -62,98 +63,72 @@ object AnswerEx3_6:
         PokemonSkill("治療", "ちりょう", "回復", 30)
   ) ) ) ) )
 
-  //def main(args: Array[String]): Unit =
-    // 既存の trainers からサトシとカスミを見つけ出す
-    //val satoshi =
-      //trainers
-        //.find(_.name == "サトシ")
-        //.get
-
-    //val kasumi =
-      //trainers
-        //.find(_.name == "カスミ")
-        //.get
-
-    // 変更した battle メソッドに Trainer オブジェクトを渡す
-    //val battleResult = battle(satoshi, kasumi, aTurn = true, turn = 1)
-
-    //println(s"=== バトル開始: ${satoshi.name} vs ${kasumi.name} ===")
-    //println(battleResult)
-    // battleResult は最終的な勝敗メッセージ（String）を返す想定なので、そのまま表示
-    //println(s"=== 決着: ${battleResult} ===")
+  def main(args: Array[String]): Unit =
+    println()
 
   /*
    *「攻撃側が 1 ターン行動した結果の (新しい攻撃側チーム, 新しい防御側チーム)」を返します。
    * 攻撃なら防御側が、回復なら攻撃側が更新されます。
    */
-  //def takeTurn(attacker: Seq[Pokemon], defender: Seq[Pokemon]): (Seq[Pokemon], Seq[Pokemon]) =
-    //val attackerPokemon =　　　　　　　　　// 攻撃するポケモン
-      //attacker                             // 攻撃側
-        //.filter(pokemon => pokemon.hp > 0) // hpが0以上
-        //.head                              // 先頭
+  def takeTurn(attacker: Seq[Pokemon], defender: Seq[Pokemon]): (Seq[Pokemon], Seq[Pokemon]) =
+    val attackerPokemon =
+      attacker
+        .head
 
-    //val defenderPokemon =                  // 防御するポケモン
-      //defender                             // 防御側
-        //.filter(pokemon => pokemon.hp > 0) // hpが0以上
-        //.head                              // 先頭
+    val defenderPokemon =
+      defender
+        .head
 
-    //val randomIndex =                        // ランダムなインデックスを取得
-      //MATH_RANDOM                            // ランダムな数値
-        //.nextInt(attackerPokemon.skill.size) // 技の数
+    val randomIndex = // ランダムなインデックスを取得
+      MATH_RANDOM
+        .nextInt(attackerPokemon.skill.size)
 
-    //val attackSkill =       // ランダムなインデックスで技を決定
-      //attackerPokemon       // 攻撃するポケモン
-        //.skill(randomIndex) // 技をランダムな数値で決定
+    val attackSkill = // ランダムなインデックスで技を決定
+      attackerPokemon
+        .skill(randomIndex)
 
-    //val newDefenderHp =                                 // 攻撃された後の防御側のポケモンのhp
-      //scala
-        //.math
-        //.max(0, defenderPokemon.hp - attackSkill.power) // 0 or hp - power
+    val newDefenderHp =
+      scala
+        .math
+        .max(0, defenderPokemon.hp - attackSkill.power)
 
-    //val updatedDefenderPokemon =  // 防御側のポケモンを更新
-      //defenderPokemon             // 防御側ポケモン
-        //.copy(hp = newDefenderHp) // hpを更新してインスタンス生成
+    val updatedDefenderPokemon =
+      defenderPokemon
+        .copy(hp = newDefenderHp)
 
-    //val newAttackerHp =                                                     // 攻撃された後の防御側のポケモンのhp
-      //scala
-        //.math
-        //.min(attackerPokemon.hpMax, attackerPokemon.hp + attackSkill.power) // hpMax or hp + 回復power
+    val newAttackerHp =
+      scala
+        .math
+        .min(attackerPokemon.hpMax, attackerPokemon.hp + attackSkill.power)
 
-    //val updatedAttackerPokemon =  // 防御側のポケモンを更新
-      //attackerPokemon             // 攻撃側のポケモン
-        //.copy(hp = newAttackerHp) // hpを更新してインスタンス生成
+    val updatedAttackerPokemon =
+      attackerPokemon
+        .copy(hp = newAttackerHp)
 
-    //val updatedDefenderTeam =                   //防御側のSeq[pokemon]の更新
-      //defenderTrainer.copy(                     //
-       // holdsPokemon =                          //
-         // defenderTrainer.holdsPokemon.updated( //
-            //0, updatedDefenderPokemon
-         // )
-     // )
+    val updatedDefenderTeam =
+      defender
+        .updated(0, updatedDefenderPokemon)
 
-    //val updatedAttackerTeam =
-      //attackerTrainer.copy(
-       // holdsPokemon =
-         // attackerTrainer.holdsPokemon.updated(
-           // 0, updatedAttackerPokemon
-          //)
-     // )
+    val updatedAttackerTeam =
+      attacker
+        .updated(0, updatedAttackerPokemon)
 
-    //if   attackSkill.kind == "攻撃" then
-      //(attackerTrainer, updatedDefenderTrainer)
-   // else
-      //(updatedAttackerTrainer, defenderTrainer)
+    (attacker, updatedDefenderTeam)
+    (updatedAttackerTeam, defender)
+
+
+
+
 
   /*
    * 決着: どちらかのチームの 全ポケモンの体力が 0（ひんし） になったら、もう一方の勝ち。
    * 引き分け: 決着がつかないまま 20 ターン を超えたら引き分け（回復ばかりで終わらない場合の保険）。
    */
-  //def battle(a: Seq[Pokemon], b: Seq[Pokemon], aTurn: Boolean, turn: Int): String =
-    //if      trainerA.holdsPokemon.forall(a => a.hp <= 0) then s"決着: トレーナーBの勝ち！" // b の勝ち
-    //else if trainerB.holdsPokemon.forall(b => b.hp <= 0) then s"決着: トレーナーAの勝ち！" // a の勝ち
-    //else if turn > 20 then "引き分け"                                                      // 決着がつかないまま 20 ターン を超えたら引き分け
-    //else
-      //val (na, nb) =
-       // if   aTurn then takeTurn(trainerA, trainerB)
-       // else takeTurn(trainerB, trainerA).swap
-      //battle(na, nb, !aTurn, turn + 1)
+
+  def battle(a: Seq[Pokemon], b: Seq[Pokemon], aTurn: Boolean, turn: Int): String =
+    if      a.forall(a => a.hp <= 0) then s"決着: トレーナーBの勝ち！" // b の勝ち
+    else if b.forall(b => b.hp <= 0) then s"決着: トレーナーAの勝ち！" // a の勝ち
+    else if turn > 20 then "引き分け"                         // 決着がつかないまま 20 ターン を超えたら引き分け
+    else
+    val (na, nb) = if aTurn then takeTurn(a, b) else takeTurn(b, a).swap
+    battle(na, nb, !aTurn, turn + 1)
