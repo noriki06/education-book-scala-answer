95825bf8b84b426d51558aa3ae8273eae9f0cdb3
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-24 10:46:59 +0900

    section9AnswerCheckpointSignup

--- src/main/scala/education/section10/Answer3.scala
+++ src/main/scala/education/section10/Answer3.scala
@@ -5,13 +5,18 @@ object Answer3:
   trait Animal:
     def name: String
     def sound: String
+    def greet: String = s"$name は $sound と鳴く"
 
   case class Dog(name: String) extends Animal:
     def sound = "ワン"
 
-  def traitQuestion1(dog: Dog): String =
-    s"${dog.name} は ${dog.sound} と鳴く"
 
+  case class Cat(name: String) extends Animal:
+    def sound = "ニャー"
+    override def greet = s"$name は気まぐれに $sound と鳴く"
+
+  def traitQuestion3(cat: Cat): String =
+    Cat("タマ").greet
 
   def main(args: Array[String]): Unit =
-    println(traitQuestion1(Dog("ポチ")))
+    println(traitQuestion3(Cat("タマ")))
