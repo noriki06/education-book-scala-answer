0aaaee7c49a0f83130b94c373af29b78e6ea7dd5
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-24 11:59:59 +0900

    section9AnswerCheckpointSignup

--- src/main/scala/education/section10/Answer3.scala
+++ src/main/scala/education/section10/Answer3.scala
@@ -16,7 +16,7 @@ object Answer3:
     override def greet = s"$name は気まぐれに $sound と鳴く"
 
   def traitQuestion3(cat: Cat): String =
-    Cat.greet
+    cat.greet
 
   def main(args: Array[String]): Unit =
     println(traitQuestion3(Cat("タマ")))
