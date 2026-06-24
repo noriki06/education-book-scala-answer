5608f8d516d1e6ae0cc3903328d5abc3b966e828
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-24 10:41:39 +0900

    section9AnswerCheckpointSignup

--- src/main/scala/education/section10/Answer2.scala
+++ src/main/scala/education/section10/Answer2.scala
@@ -1,1 +1,21 @@
+package education.section10
 
+object Answer2:
+
+  trait Animal:
+    def name: String
+    def sound: String
+    def greet: String = s"$name は $sound と鳴く"
+
+  case class Dog(name: String) extends Animal:
+    def sound = "ワン"
+
+
+  case class Cat(name: String) extends Animal:
+    def sound = "ニャー"
+
+  def traitQuestion2(): Seq[String] =
+    Seq(Dog("ポチ").greet, Cat("タマ").greet)
+
+  def main(args: Array[String]): Unit =
+    println(traitQuestion2())
