9ac60b2c4584fb72e5a078396d1032b30b3fb3f7
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-25 10:08:06 +0900

    section0Ansewer1

---
+++ src/main/scala/education/section0/Answer1.scala
@@ -0,0 +1,11 @@
+package education.section0
+
+object Answer1:
+
+  def add(a: Int, b: Int): Int =
+    a + b
+
+
+  def main(args: Array[String]): Unit =
+    println(add(2, 3))
+    println(add(-1, 1))
