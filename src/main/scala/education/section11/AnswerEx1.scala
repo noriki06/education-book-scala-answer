a6f23c8a2b7a07275337903939a937917388c2ae
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-26 16:11:21 +0900

    section11Ansewer1

---
+++ src/main/scala/education/section11/AnswerEx1.scala
@@ -0,0 +1,26 @@
+package education.section11
+
+object AnswerEx1:
+
+  trait Combine[A]:
+    def empty: A                    // 「何もない」状態（合計の 0、連結の "" にあたる）
+    def combine(x: A, y: A): A
+
+  given Combine[Int]:
+    empty = 0
+    combine = x + y
+
+  given Combine[String]:
+    empty = ""
+    combine = x + y
+
+
+
+  def combineQuestion1(): Seq[String] =
+    Seq(
+      summon[Combine[Int]].combine(3, 4)
+      summon[Combine[String]].combine("ab", "cd")
+    )
+
+  def main(args: Array[String]): Unit =
+    println(combineQuestion1())
