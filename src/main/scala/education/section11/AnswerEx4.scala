c809c204be1af5ba7d604e677d1ffdc886f4bbca
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-26 16:51:26 +0900

    section11Ansewer1

---
+++ src/main/scala/education/section11/AnswerEx4.scala
@@ -0,0 +1,14 @@
+package education.section11
+
+object AnswerEx4:
+
+  case class Member(name: String, score: Int)
+
+  given Ordering[Member] =
+    Ordering.by[Member, Int](_.score).reverse
+
+  def orderingQuestion(members: Seq[Member]): Seq[Member] =
+    members.sorted
+
+  def main(args: Array[String]): Unit =
+    println(orderingQuestion(Seq(Member("A", 50), Member("B", 90), Member("C", 70))))
