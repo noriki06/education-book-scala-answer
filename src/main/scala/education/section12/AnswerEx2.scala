aa7439b082edc921a31b4c822528c94fab28970a
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-06-29 16:28:02 +0900

    section11Ansewer1

--- src/main/scala/education/section12/AnswerEx2.scala
+++ src/main/scala/education/section12/AnswerEx2.scala
@@ -8,6 +8,9 @@ object AnswerEx2:
   import cats.data.EitherT
   import cats.syntax.all.*
 
+  /**
+   *注文のケースクラス
+   */
   case class Order(
     id: Int,
     customerId: Int,
@@ -15,28 +18,41 @@ object AnswerEx2:
     quantity: Int
   )
 
+  /**
+   * 顧客のケースクラス
+   */
   case class Customer(
     id: Int,
     name: String
   )
 
+  /**
+   *商品のケースクラス
+   */
   case class Product(
     id: Int,
     name: String,
     price: Int
   )
 
-  enum ErrorType:
-    case NoneOrder
-    case NoneCustomer
-    case NoneProduct
-
+  /**
+   * 注文詳細のケースクラス
+   */
   case class Details(
     customerName: String,
     productName: String,
     amount: String,
     totalAmount: String
   )
+
+  /**
+   * エラーのタイプ
+   */
+  enum ErrorType:
+    case NoneOrder(id: Int)
+    case NoneCustomer(id: Int)
+    case NoneProduct(id: Int)
+
 
   def main(args: Array[String]): Unit =
     val orders: Map[Int, Order] =
@@ -62,30 +78,42 @@ object AnswerEx2:
     val result4: Either[ErrorType, Details] = Await.result(detailsByOrderId(999, orders, customers, products), Duration.Inf)
     println(result4)
 
+  /**
+   * orders を order_id で引くメソッド
+   */
   def orderToMap(orders: Map[Int, Order], id: Int): Future[Either[ErrorType, Order]] =
     Future {
       Thread.sleep(500)
       orders
         .get(id)
-          .toRight(ErrorType.NoneOrder)
+          .toRight(ErrorType.NoneOrder(id))
     }
 
+  /**
+   * customers を customer_id で引くメソッド
+   */
   def customerToMap(customers: Map[Int, Customer], id: Int): Future[Either[ErrorType, Customer]] =
     Future {
       Thread.sleep(500)
       customers
         .get(id)
-          .toRight(ErrorType.NoneCustomer)
+          .toRight(ErrorType.NoneCustomer(id))
     }
 
+  /**
+   * products を product_id で引くメソッド
+   */
   def productToMap(products: Map[Int, Product], id: Int): Future[Either[ErrorType, Product]] =
     Future {
       Thread.sleep(500)
       products
         .get(id)
-          .toRight(ErrorType.NoneProduct)
+          .toRight(ErrorType.NoneProduct(id))
     }
 
+  /**
+   * 顧客・商品・数量から 注文詳細を組み立てるメソッド
+   */
   def detailsByOrderId(
     orderId: Int,
     orders: Map[Int, Order],
