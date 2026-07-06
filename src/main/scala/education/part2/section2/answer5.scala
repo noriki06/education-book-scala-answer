1719ccc80f49f5eabb66af81a939faa8b26a2da0
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-07-06 16:17:04 +0900

    domain

--- src/main/scala/education/part2/section2/answer5.scala
+++ src/main/scala/education/part2/section2/answer5.scala
@@ -1,14 +1,22 @@
 package education.part2.section2
 
-import ixias.core.model.*
+import ixias.core.model.*          // 状態の find / of を使う問題では ixias.core.model.syntax.* も
+
 object Answer5:
 
+  case class Member(
+    id: Option[Member.Id],
+    name: String,
+    updatedAt: LocalDateTime = Now,
+    createdAt: LocalDateTime = Now
+  ) extends EntityModel[Member.Id]
+
   object Member:
-    object Id extends EntityId[Long]
+    object Id extends EntityId[Long]     // Long を土台にした「会員IDの工場」
     type Id = Id.Repr
+    type WithNoId = Entity.WithNoId[Id, Member]
 
   def main(args: Array[String]): Unit =
-    val id: Member.Id = Member.Id(1001L)
+    Member(id = None, name = "Alice").toWithNoId         // apply で生成
 
-    println(Member.Id(1001L).value)
-    println(Member.Id(1001L).asString)
+    println(draft.v.name)
