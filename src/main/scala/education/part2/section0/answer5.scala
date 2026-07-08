1ae75badc8f4878064ff22dd838e37472a6222ab
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-07-08 10:35:50 +0900

    domain

--- src/main/scala/education/part2/section0/answer5.scala
+++ src/main/scala/education/part2/section0/answer5.scala
@@ -2,12 +2,22 @@ package education.part2.section0
 
 import ixias.core.model.*
 
-object Answer1:
+object Answer5:
+
+  case class Member(
+    id:        Option[Member.Id],
+    name:      String,
+    updatedAt: LocalDateTime = Now,
+    createdAt: LocalDateTime = Now
+  ) extends EntityModel[Member.Id]
 
   object Member:
     object Id extends EntityId[Long]
     type Id = Id.Repr
+    type WithNoId = Entity.WithNoId[Id, Member]
 
   def main(args: Array[String]): Unit =
-    println(Member.Id(1001L).value)
-    println(Member.Id(1001L).asString)
+    val draft = Member(id = None, name = "Alice").toWithNoId
+
+
+    println(draft.v.name)
