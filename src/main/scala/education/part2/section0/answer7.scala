4ab61f6c0cd4d18362d3b7674fdcd0b3426eaf17
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-07-08 10:55:59 +0900

    domain

--- src/main/scala/education/part2/section0/answer7.scala
+++ src/main/scala/education/part2/section0/answer7.scala
@@ -1,13 +1,13 @@
 package education.part2.section0
 
 import ixias.core.model.*
-import ixias.core.model.syntax.*
 
 object Answer7:
 
   case class Member(
     id:        Option[Member.Id],
     name:      String,
+    state:     Member.Status,
     updatedAt: LocalDateTime = Now,
     createdAt: LocalDateTime = Now
   ) extends EntityModel[Member.Id]
@@ -15,6 +15,8 @@ object Answer7:
   object Member:
     object Id extends EntityId[Long]
     type Id = Id.Repr
+    type WithNoId = Entity.WithNoId[Id, Member]
+    type EmbeddedId = Entity.EmbeddedId[Id, Member]
 
     enum Status(val code: Int) extends EnumStatus[Int]:
       case Active    extends Status(1)
@@ -22,6 +24,8 @@ object Answer7:
       case Withdrawn extends Status(3)
 
   def main(args: Array[String]): Unit =
-    println(Member.Status.of(2))
-    println(Member.Status.find(99))
-    println(Member.Status.Active.code)
+    val saved = Member(id = Some(Member.Id(1L)), name = "Alice", state = Member.Status.Active).toEmbeddedId
+    val suspended = saved.map(_.copy(state = Member.Status.Suspended))
+
+    println(suspended.id.asString)
+    println(suspended.v.state)
