22c20ef198bf5d3618c4b3b79630f43084caed0d
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-07-08 12:13:11 +0900

    domain

--- src/main/scala/education/part2/section0/answer13.scala
+++ src/main/scala/education/part2/section0/answer13.scala
@@ -8,18 +8,47 @@ object Answer13:
     id: Int,
     name: String,
     groupId: Int,
-    groupName: Option[String] = None
+    groupName: Option[String] = None,
+    postCount: Int = 0
   ) extends JoinSyntax[Member]
-
-  object Member:
-    given JoinWith[Member, Group] =
-      (member, groups) =>
-        member.copy(groupName = groups.find(_.id == member.groupId).map(_.name))
 
   case class Group(
     id: Int,
     name: String
   )
 
+  case class Post(
+    id: Int,
+    memberId: Int,
+    title: String
+  )
+
+  object Member:
+    given JoinWith[Member, Group] =
+      (member, groups) =>
+        member.copy(groupName = groups.find(_.id == member.groupId).map(_.name))
+
+    given JoinWith[Member, Post] =
+      (member, posts) =>
+        member.copy(postCount = posts.count(_.memberId == member.id))
+
+
   def main(args: Array[String]): Unit =
-    println(Member(1, "Alice", 10).join(Seq(Group(10, "営業"), Group(20, "開発"))))
+    val members = Seq(
+      Member(1, "Alice", 10),
+      Member(2, "Bob", 20)
+    )
+
+    val allGroups = Seq(
+      Group(10, "営業"),
+      Group(20, "開発")
+    )
+
+    val allPosts = Seq(
+      Post(10, 1, "これ"),
+      Post(20, 1, "それ"),
+      Post(30, 1, "あれ"),
+      Post(40, 2, "違う")
+    )
+
+    println(members.map(_.join(allGroups).join(allPosts)))
---
+++ src/main/scala/education/part2/section0/answer15.scala
@@ -0,0 +1,12 @@
+package education.part2.section0
+
+import ixias.core.model.*
+import ixias.core.model.syntax.*
+
+object Answer15:
+
+  def main(args: Array[String]): Unit =
+    val xs = List(Some(3), None, Some(1), Some(2))
+
+
+    println(xs.sorted(using summon[Ordering[Int]].noneLastDesc))
