740367c4c19fa395c13db903b3b1d5d351db5872
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-07-21 09:38:34 +0900

    ixias

--- src/main/scala/education/part2/section4/shift/function/CoreTime.scala
+++ src/main/scala/education/part2/section4/shift/function/CoreTime.scala
@@ -5,47 +5,49 @@ import java.time.{Duration, LocalDate, LocalDateTime}
 /**
  * 通常帯(05:00～22:00)の勤務時間を求める
  */
-object CalculateNormalTime
+object CoreTime
   extends Function2[LocalDateTime, LocalDateTime, Duration]:
 
   def apply(
-    start: LocalDateTime,
-    end: LocalDateTime
+      start: LocalDateTime,
+      end: LocalDateTime
   ): Duration =
 
-    // 合計時間
-    var total = Duration.ZERO
-
-    // 勤務開始日
-    var day: LocalDate = start.toLocalDate
-
-    // 勤務終了日
     val lastDay = end.toLocalDate
 
-    // 開始日～終了日まで1日ずつ計算
-    while !day.isAfter(lastDay) do
+    def loop(
+        day: LocalDate,
+        total: Duration
+    ): Duration =
 
-      // その日の通常帯
-      val normalStart = day.atTime(5, 0)
-      val normalEnd   = day.atTime(22, 0)
+      if day.isAfter(lastDay) then
+        total
 
-      // 実勤務との重なり開始
-      val overlapStart =
-        if start.isAfter(normalStart) then start
-        else normalStart
+      else
 
-      // 実勤務との重なり終了
-      val overlapEnd =
-        if end.isBefore(normalEnd) then end
-        else normalEnd
+        val normalStart = day.atTime(5, 0)
+        val normalEnd   = day.atTime(22, 0)
 
-      // 重なっていれば加算
-      if overlapStart.isBefore(overlapEnd) then
-        total = total.plus(
-          Duration.between(overlapStart, overlapEnd)
+        val overlapStart =
+          if start.isAfter(normalStart) then start
+          else normalStart
+
+        val overlapEnd =
+          if end.isBefore(normalEnd) then end
+          else normalEnd
+
+        val added =
+          if overlapStart.isBefore(overlapEnd) then
+            Duration.between(overlapStart, overlapEnd)
+          else
+            Duration.ZERO
+
+        loop(
+          day.plusDays(1),
+          total.plus(added)
         )
 
-      // 次の日へ
-      day = day.plusDays(1)
-
-    total
+    loop(
+      start.toLocalDate,
+      Duration.ZERO
+    )
