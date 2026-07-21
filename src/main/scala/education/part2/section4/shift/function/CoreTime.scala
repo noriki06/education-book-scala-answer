29a2b96fe7d8caa21bda5c8951a412f3ba6f1993
Noriki Ohira <n.ohira@hybrid-technologies.co.jp>
2026-07-21 11:15:14 +0900

    ixias

--- src/main/scala/education/part2/section4/shift/function/CoreTime.scala
+++ src/main/scala/education/part2/section4/shift/function/CoreTime.scala
@@ -1,11 +1,8 @@
 package education.part2.section4.shift.function
 
-import java.time.{Duration, LocalDate, LocalDateTime}
+import java.time.{Duration, LocalDate}
 import education.part2.section4.shift.model.WorkBreakdown
 
-/**
- * 通常帯(05:00～22:00)の勤務時間を求める
- */
 object CoreTime
   extends Function2[LocalDateTime, LocalDateTime, Duration]:
 
@@ -13,35 +10,32 @@ object CoreTime
       start: LocalDateTime,
       end: LocalDateTime
   ): Duration =
+
     val firstDay = start.toLocalDate
+    val lastDay  = end.toLocalDate
 
-    val lastDay = end.toLocalDate
+    // 開始日～終了日までの日付を作る
+    val days = Duration.between(firstDay, lastDay)
 
-    def loop(day: LocalDate, total: Duration): Duration =
-      if firstDay == lastDay then total
-      else
-        val normalStart = day.atTime(WorkBreakdown.NormalStart)
-        val normalEnd   = day.atTime(WorkBreakdown.NormalEnd)
-        val overlapStart =
-          if start.isAfter(normalStart) then start
-          else normalStart
+    // 1日ずつ通常帯を計算して合計する
+    days.foldLeft(Duration.ZERO) { (total, day) =>
 
-        val overlapEnd =
-          if end.isBefore(normalEnd) then end
-          else normalEnd
+      val normalStart = day.atTime(WorkBreakdown.NormalStart)
+      val normalEnd   = day.atTime(WorkBreakdown.NormalEnd)
 
-        val added =
-          if overlapStart.isBefore(overlapEnd) then
-            Duration.between(overlapStart, overlapEnd)
-          else
-            Duration.ZERO
+      val overlapStart =
+        if start.isAfter(normalStart) then start
+        else normalStart
 
-        loop(
-          day.plusDays(1),
-          total.plus(added)
-        )
+      val overlapEnd =
+        if end.isBefore(normalEnd) then end
+        else normalEnd
 
-    loop(
-      start.toLocalDate,
-      Duration.ZERO
-    )
+      val added =
+        if overlapStart.isBefore(overlapEnd) then
+          Duration.between(overlapStart, overlapEnd)
+        else
+          Duration.ZERO
+
+      total.plus(added)
+    }
